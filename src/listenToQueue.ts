6bff7ae4b48d7fc0e7936a872e810cf8ea203b0d
James Saballegue <jp.saballegue@gmail.com>
2024-02-07 18:34:03 +0800

    fix: catch errors on listen

--- src/listenToQueue.ts
+++ src/listenToQueue.ts
@@ -10,24 +10,29 @@ export const listenToQueue = async (queue: string) => {
     throw new Error("JWT_MODS_SECRET not found in env");
   }
 
-  const conn = await amqplib.connect(process.env.RABBITMQ_URL, {
-    clientProperties: { connection_name: "JobBoardNotifService" },
-  });
+  try {
+    const conn = await amqplib.connect(process.env.RABBITMQ_URL, {
+      clientProperties: { connection_name: "JobBoardNotifService" },
+    });
 
-  const ch1 = await conn.createChannel();
-  await ch1.assertQueue(queue, {
-    durable: true,
-  });
+    const ch1 = await conn.createChannel();
+    await ch1.assertQueue(queue, {
+      durable: true,
+    });
 
-  ch1.consume(queue, async (msg) => {
-    if (!msg) return;
+    ch1.consume(queue, async (msg) => {
+      if (!msg) return;
 
-    await notifyModerators(msg);
+      await notifyModerators(msg);
 
-    ch1.ack(msg);
-  });
+      ch1.ack(msg);
+    });
 
-  console.log("listening to queue", queue);
+    console.log("listening to queue", queue);
 
-  return conn;
+    return conn;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
